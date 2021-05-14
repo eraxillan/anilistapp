e6fbdc5248229e2d4219acf30d442c187bfa26c9
Aleksandr Kamyshnikov <5300377+eraxillan@users.noreply.github.com>
2021-05-14 13:33:04 +0300

    Refactoring: Navigation Component have not tablet support, so remove it

--- app/src/main/java/name/eraxillan/ongoingschedule/ui/OngoingListActivity.kt
+++ app/src/main/java/name/eraxillan/ongoingschedule/ui/OngoingListActivity.kt
@@ -1,6 +1,7 @@
 package name.eraxillan.ongoingschedule.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import name.eraxillan.ongoingschedule.*
 import name.eraxillan.ongoingschedule.databinding.ActivityOngoingListBinding
@@ -18,14 +19,11 @@ class OngoingListActivity
 
     private lateinit var binding: ActivityOngoingListBinding
     private var ongoingDetailsFragment: OngoingDetailsFragment? = null
-    private var largeScreen = false
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
     private fun showOngoingInfo(ongoing: Ongoing) {
         title = ongoing.originalName
-
-        val id = if (largeScreen) R.id.fragment_container else R.id.nav_host_fragment_container_view
 
         ongoingDetailsFragment = OngoingDetailsFragment.newInstance(ongoing)
 
@@ -33,7 +31,7 @@ class OngoingListActivity
             supportFragmentManager
                 .beginTransaction()
                 .replace(
-                    id, it,
+                    R.id.nav_host_fragment_container_view, it,
                     getString(R.string.ongoing_detail_fragment_tag)
                 )
                 .addToBackStack(null)
@@ -54,13 +52,6 @@ class OngoingListActivity
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbarMain)
-
-        // `FragmentManager` is an object that
-        // lets you dynamically add and remove Fragments at runtime. This gives you a
-        // powerful tool to make the UI as flexible as possible across various screen sizes.
-        // It's called a support Fragment manager rather than just a Fragment manager because
-        // some older versions of Android didn't include fragments
-        largeScreen = (binding.contentMain.fragmentContainer != null)
     }
 
     // Called when the activity has detected the user's press of the back key
@@ -89,7 +80,6 @@ class OngoingListActivity
     }
 
     override fun onOngoingListReloaded(firstOngoing: Ongoing) {
-        if (largeScreen)
-            showOngoingInfo(firstOngoing)
+        Toast.makeText(this, "Ongoing list was reloaded", Toast.LENGTH_LONG).show()
     }
 }
